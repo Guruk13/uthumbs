f79f1c66b88e51d26dfc4af38b5ce52e070ca3e8
Matéo <mateo.potin@etu.u-bordeaux.fr>
2020-02-06 15:36:51 +0100

    ajout map

--- Map.js
+++ Map.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import { View, Text, Button } from 'react-native';
+import MapView from 'react-native-maps'
 
 export class Map extends Component {
+
+    state = {
+      latitude: null,
+      longitude: null,
+      routeCoordinates: [],
+      distanceTravelled: 0,
+      prevLatLng: {},
+      //coordinate: new AnimatedRegion({
+       //latitude: null,
+       //longitude: null
+      //})
+    };
+  
   render() {
     return (
-      <View>
-        <Text>This is the Map screen</Text>
-        <Button onPress={() => this.props.navigation.navigate('Home')} title="Go home"/>
-      </View>
-    )
+        <MapView        style={{flex: 1}}        region={{          latitude: 42.882004,          longitude: 74.582748        }}        showsUserLocation={true}      />
+    );
   }
 };
 
