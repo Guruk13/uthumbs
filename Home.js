3a2b8bb8ca4b6126385e8aba5d06a630ef7415fd
Paul <paul.demets@etu.u-bordeaux.fr>
2020-02-06 15:37:32 +0100

    conflit

--- Home.js
+++ Home.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+
 
 export class Home extends Component {
   render() {
     return (
-      <View>
-        <Text>This is the Settings screen</Text>
-        <Button onPress={() => this.props.navigation.navigate('Map')} title="Go to map"/>
+      <View style={styles.container}>
+        <Text>Aujourd'hui vous vous sentez plutôt : </Text>
+        <Button onPress={() => this.props.navigation.navigate('Map')} title="Piéton" />
+        
+        <Button onPress={() => this.props.navigation.navigate('Map')} title="Conducteur" />
+
       </View>
     )
   }
 };
 
 export default Home;
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    marginTop: '200'
+
+  }
+});
