72f9860962bdd06cae06081282cec36ea8aeea8f
Paul <paul.demets@etu.u-bordeaux.fr>
2020-02-06 11:07:33 +0100

    z

--- App.js
+++ App.js
@@ -1,29 +1,16 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import AppNavigator from './AppNavigator';
 
-import Home from './Home';
-import myMap from './Map';
-
-export default class App extends Component {
+export default class App extends React.Component {
   render() {
-    return <AppContainer />;
+    return (
+      <View style={styles.container}>
+        <Text>Hello</Text>
+      </View>
+    );
   }
 }
-
-const AppNavigator = createStackNavigator({
-  Home: {
-    screen: Home
-  },
-  myMap: {
-    screen: myMap
-  },
-}, {
-  initialRouteName: "Home"
-});
-
-const AppContainer = createAppContainer(AppNavigator);
-
 
 const styles = StyleSheet.create({
   container: {
