fbf29c8f9485b1e6a552d2a47616ce4baecaaf45
Matéo <mateo.potin@etu.u-bordeaux.fr>
2020-02-07 15:32:23 +0100

    ajout marqueur + route

--- Map.js
+++ Map.js
@@ -1,26 +1,98 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
-import MapView from 'react-native-maps'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import { AppRegistry, View, StyleSheet, Dimensions } from 'react-native';
+import MapViewDirections from 'react-native-maps-directions';
+
+const { width, height } = Dimensions.get('window');
+const ASPECT_RATIO = width / height;
+const LATITUDE = 37.78825;
+const LONGITUDE = -122.4324;
+const LATITUDE_DELTA = 0.0122;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+const SPACE = 0.01;
+const GOOGLE_MAPS_APIKEY = 'AIzaSyB2sGMhio_-YehtPloM5a2qjFnojLzil2k';
 
 export class Map extends Component {
+  constructor() {
+    super();
+    this.state = {
+      region: {
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA
+      },
+      origin: {
+        latitude: LATITUDE,
+        longitude: LONGITUDE
+      },
+      destination: {
+        latitude: 44.802928,
+        longitude: -0.614873
+      }
+    }
+  }
 
-    state = {
-      latitude: null,
-      longitude: null,
-      routeCoordinates: [],
-      distanceTravelled: 0,
-      prevLatLng: {},
-      //coordinate: new AnimatedRegion({
-       //latitude: null,
-       //longitude: null
-      //})
-    };
-  
-  render() {
-    return (
-        <MapView        style={{flex: 1}}        region={{          latitude: 42.882004,          longitude: 74.582748        }}        showsUserLocation={true}      />
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          region: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          },
+          origin:{
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          }
+        });
+      },
+      (error) => alert(error.message),
+      { timeout: 100000 }
     );
   }
-};
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={styles.map}
+          region={this.state.region}
+          showsUserLocation={true}
+          followsUserLocation={true}>
+          <MapView.Marker
+            coordinate={
+             this.state.destination
+            }
+            title={"title"}
+            description={"description"}
+          />
+          <MapViewDirections
+            origin={this.state.origin}
+            destination={this.state.destination}
+            apikey={GOOGLE_MAPS_APIKEY}
+            strokeWidth={5}
+            strokeColor="#a64253"
+          />
+        </MapView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
+
 
 export default Map;
