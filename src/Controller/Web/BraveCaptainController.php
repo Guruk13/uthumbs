d040078917be824a2d197006d8e92afc184182c3
marel <marwane.elaouni@gmail>
2020-02-11 16:21:49 +0100

    Controller

---
+++ src/Controller/Web/BraveCaptainController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BraveCaptainController extends AbstractController
+{
+    /**
+     * @Route("/brave/captain", name="brave_captain")
+     */
+    public function index()
+    {
+        return $this->render('brave_captain/index.html.twig', [
+            'controller_name' => 'BraveCaptainController',
+        ]);
+    }
+}
