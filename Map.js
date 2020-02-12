4c7b04958a85f6cf009a36a0292f7cb94011fe48
Matéo <mateo.potin@etu.u-bordeaux.fr>
2020-02-12 10:12:40 +0100

    ajout geolocalisation temps réel

--- Map.js
+++ Map.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-import { AppRegistry, View, StyleSheet, Dimensions } from 'react-native';
+import MapView, { PROVIDER_GOOGLE, AnimatedRegion, Marker } from 'react-native-maps';
+import { AppRegistry, View, StyleSheet, Dimensions, Button, Text } from 'react-native';
 import MapViewDirections from 'react-native-maps-directions';
 import { connect } from 'react-redux'
+import PubNubReact from "pubnub-react";
 
 const { width, height } = Dimensions.get('window');
 const ASPECT_RATIO = width / height;
@@ -17,38 +18,83 @@ class Map extends Component {
   constructor() {
     super();
     this.state = {
-      region: {
+      latitude: LATITUDE,
+      longitude: LONGITUDE,
+      coordinate: new AnimatedRegion({
         latitude: LATITUDE,
         longitude: LONGITUDE,
-        latitudeDelta: LATITUDE_DELTA,
-        longitudeDelta: LONGITUDE_DELTA
-      },
-      origin: {
-        latitude: LATITUDE,
-        longitude: LONGITUDE
-      }
-    }
+        latitudeDelta: 0,
+        longitudeDelta: 0
+      })
+    };
+
+    this.pubnub = new PubNubReact({
+      publishKey: "pub-c-4e1007f6-a370-4538-8465-630ea4cc6ab6",
+      subscribeKey: "sub-c-b6c24fd6-4ce6-11ea-80a4-42690e175160"
+    });
+    this.pubnub.init(this);
   }
 
   componentDidMount() {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({
-          region: {
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
-            latitudeDelta: LATITUDE_DELTA,
-            longitudeDelta: LONGITUDE_DELTA,
-          },
-          origin: {
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
+    this.watchLocation();
+  }
+
+  watchLocation = () => {
+    const { coordinate } = this.state;
+
+    this.watchID = navigator.geolocation.watchPosition(
+      position => {
+        const { latitude, longitude } = position.coords;
+
+        const newCoordinate = {
+          latitude,
+          longitude,
+        };
+
+        if (Platform.OS === 'android') {
+          if (this.marker) {
+            this.marker._component.animateMarkerToCoordinate(newCoordinate, 500); // 500 is the duration to animate the marker
           }
+        } else {
+          coordinate.timing(newCoordinate).start();
+        }
+
+        this.setState({
+          latitude,
+          longitude,
         });
       },
-      (error) => alert(error.message),
-      { timeout: 100000 }
+      error => console.log(error),
+      {
+        enableHighAccuracy: true,
+        timeout: 20000,
+        maximumAge: 0,
+        distanceFilter: 1,
+      }
     );
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.latitude !== prevState.latitude) {
+      this.pubnub.publish({
+        message: {
+          latitude: this.state.latitude,
+          longitude: this.state.longitude,
+        },
+        channel: 'location',
+      });
+    }
+  }
+
+  getMapRegion = () => ({
+    latitude: this.state.latitude,
+    longitude: this.state.longitude,
+    latitudeDelta: LATITUDE_DELTA,
+    longitudeDelta: LONGITUDE_DELTA
+  });
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
   }
 
   render() {
@@ -57,9 +103,10 @@ class Map extends Component {
         <MapView
           provider={PROVIDER_GOOGLE}
           style={styles.map}
-          region={this.state.region}
+          region={this.getMapRegion()}
           showsUserLocation={true}
-          followsUserLocation={true}>
+          followsUserLocation={true}
+          onRegionChange={this._handleMapRegionChange}>
           <MapView.Marker
             coordinate={
               this.props.destination
@@ -67,14 +114,27 @@ class Map extends Component {
             title={"title"}
             description={"description"}
           />
+          <Marker.Animated
+            ref={marker => {
+              this.marker = marker;
+            }}
+            coordinate={this.state.coordinate}
+          />
           <MapViewDirections
-            origin={this.state.origin}
+            origin={{latitude: this.state.latitude, longitude: this.state.longitude}}
             destination={this.props.destination}
             apikey={GOOGLE_MAPS_APIKEY}
             strokeWidth={5}
             strokeColor="#a64253"
           />
         </MapView>
+        <View style={styles.button}>
+          <Button
+            title="GO"
+            color="white"
+            onPress={() => this.setState({ followsUserLocation: true })}
+          />
+        </View>
       </View>
     );
   }
@@ -82,13 +142,24 @@ class Map extends Component {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    flexDirection: 'column',
     ...StyleSheet.absoluteFillObject,
-    justifyContent: 'flex-end',
     alignItems: 'center',
   },
   map: {
     ...StyleSheet.absoluteFillObject,
   },
+  button: {
+    flex: 1,
+    alignSelf: 'center',
+    position: 'absolute',
+    bottom: "20%",
+    backgroundColor: "#a64253",
+    width: 250,
+    borderRadius: 10
+
+  }
 });
 
 const mapStateToProps = (state) => {
