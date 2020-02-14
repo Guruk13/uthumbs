e8ad8eee90267692d52d215e07b7a94127b704d2
marel <marwane.elaouni@gmail>
2020-02-14 15:08:45 +0100

    put , delete get by id

--- src/Controller/Rest/LocationController.php
+++ src/Controller/Rest/LocationController.php
@@ -16,12 +16,10 @@ use Symfony\Component\Serializer\Encoder\XmlEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
 
-
-
-
-
 class LocationController  extends FOSRestController
 {
+
+
     /**
      * Creates an Location resource
      * @Rest\Post("/locations")
@@ -30,15 +28,15 @@ class LocationController  extends FOSRestController
      */
     public function postLocation(Request $request): View
     {
+
         //encode/decode 
         $encoders = [new XmlEncoder(), new JsonEncoder()];
         $normalizers = [new ObjectNormalizer()];
         $serializer = new Serializer($normalizers, $encoders);
 
-
         $location = new Location();
-        $okaydumper =  $request->getContent();
-        $location = $serializer->deserialize($okaydumper, Location::class, 'json');
+        $jsonContent =  $request->getContent();
+        $location = $serializer->deserialize($jsonContent, Location::class, 'json');
         $entityManager = $this->getDoctrine()->getManager();
         $entityManager->persist($location);
 
@@ -57,7 +55,56 @@ class LocationController  extends FOSRestController
         $repository = $this->getDoctrine()->getRepository(Location::class);
         $locations =  $repository->findAll();
 
-        // In case our GET was a success we need to return a 200 HTTP OK response with the collection of article object
+        // In case our GET was a success we need to return a 200 HTTP OK response with the collection of location object
         return View::create($locations, Response::HTTP_OK);
+    }
+
+    /**
+     * Retrieves an Location resource
+     * @Rest\Get("/locations/{locationId}")
+     */
+    public function getLocation(int $locationId): View
+    {
+        $repository = $this->getDoctrine()->getRepository(Location::class);
+        $location = $repository->findById($locationId);
+        // In case our GET was a success we need to return a 200 HTTP OK response with the request object
+        return View::create($location, Response::HTTP_OK);
+    }
+    /**
+     * Replaces Location resource
+     * @Rest\Put("/locations/{locationId}")
+     */
+    public function putLocation(int $locationId, Request $request): View
+    {
+        $repository = $this->getDoctrine()->getRepository(Location::class);
+        $location = $repository->findById($locationId);
+        if ($location) {
+            //encode/decode 
+            $encoders = [new XmlEncoder(), new JsonEncoder()];
+            $normalizers = [new ObjectNormalizer()];
+            $serializer = new Serializer($normalizers, $encoders);
+            $jsonContent = $request->getContent();;
+            $location = $serializer->deserialize($jsonContent, Location::class, 'json');
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($location);
+            $entityManager->flush();
+        }
+        // In case our PUT was a success we need to return a 200 HTTP OK response with the object as a result of PUT
+        return View::create($location, Response::HTTP_OK);
+    }
+
+    /**
+     * Removes the Location resource
+     * @Rest\Delete("/locations/{locationId}")
+     */
+    public function deleteLocation(int $locationId): View
+    {
+        $location = $this->locationRepository->findById($locationId);
+        if ($location) {
+            $this->locationRepository->delete($location);
+        }
+        // In case our DELETE was a success we need to return a 204 HTTP NO CONTENT response. The object is deleted.
+        return View::create([], Response::HTTP_NO_CONTENT);
     }
 }
--- src/Controller/Rest/RideController.php
+++ src/Controller/Rest/RideController.php
@@ -1,44 +1,63 @@
 <?php
-//https://www.thinktocode.com/2018/03/26/symfony-4-rest-api-part-1-fosrestbundle/
+
 namespace App\Controller\Rest;
 //Business
 use App\Entity\Ride;
 //Technical 
 use Symfony\Component\Routing\Annotation\Route;
 use FOS\RestBundle\Controller\FOSRestController;
-use FOS\RestBundle\Controller\Annotations as Rest ;
+use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use FOS\RestBundle\View\View;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+
+
 
 
 class RideController  extends FOSRestController
 {
-        /**
-         * Creates an Ride resource
-         * @Rest\Post("/rides")
-         * @param Request $request
-         * @return View
-         */
-        public function postRide(Request $request): View
-        {
-            $article = new Ride();
-            $article->setPassengerRoom($request->get('passengerVolume'));
-            $this->articleRepository->save($article);
-            // In case our POST was a success we need to return a 201 HTTP CREATED response
-            return View::create($article, Response::HTTP_CREATED);
-        }
-
     /**
-     * Retrieves a collection of Ride resource
-     * @Rest\Get("/rides")
+     * Creates an Location resource
+     * @Rest\Post("/rides")
+     * @param Request $request
+     * @return View 
      */
-    public function getRides(): View
+    public function postLocation(Request $request): View
     {
-        $articles = $this->rideRepository->findAll();
-        // In case our GET was a success we need to return a 200 HTTP OK response with the collection of article object
-        return View::create($articles, Response::HTTP_OK);
+        //encode/decode 
+        $encoders = [new XmlEncoder(), new JsonEncoder()];
+        $normalizers = [new ObjectNormalizer()];
+        $serializer = new Serializer($normalizers, $encoders);
+
+
+        $location = new Ride();
+        $okaydumper =  $request->getContent();
+        $location = $serializer->deserialize($okaydumper, Ride::class, 'json');
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($location);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+        return View::create($location, Response::HTTP_CREATED);
     }
 
+    /**
+     * Retrieves a collection of Location resource
+     * @Rest\Get("/locations")
+     */
+    public function getLocations(): View
+    {
 
+        $repository = $this->getDoctrine()->getRepository(Location::class);
+        $locations =  $repository->findAll();
+
+        // In case our GET was a success we need to return a 200 HTTP OK response with the collection of article object
+        return View::create($locations, Response::HTTP_OK);
+    }
 }
